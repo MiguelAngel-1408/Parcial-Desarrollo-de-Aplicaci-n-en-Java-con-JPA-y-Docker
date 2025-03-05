8c8fe83162a2ebb56ccb194976f35f64f959ead5
MiguelAngel-1408 <patino.miguel.0232@eam.edu.co>
2025-03-05 17:38:59 -0500

    feat: brand repository added

---
+++ src/main/java/com/proyect/parcial/repositories/IBrandRepostory.java
@@ -0,0 +1,10 @@
+package com.proyect.parcial.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.proyect.parcial.models.Brand;
+
+public interface IBrandRepostory extends JpaRepository<Brand, Long> {
+
+    
+} 
