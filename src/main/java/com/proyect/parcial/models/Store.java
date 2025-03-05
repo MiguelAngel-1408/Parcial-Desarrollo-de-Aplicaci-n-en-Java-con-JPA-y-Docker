5b6b5a66fead05c632b4bbe7fd9d0e2356cd80d2
MiguelAngel-1408 <patino.miguel.0232@eam.edu.co>
2025-03-05 16:54:58 -0500

    feat: Class store completed

---
+++ src/main/java/com/proyect/parcial/models/Store.java
@@ -0,0 +1,67 @@
+package com.proyect.parcial.models;
+
+import java.util.List;
+
+// Importing necessary JPA annotations for entity mapping
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+
+
+// Defining the class as a JPA entity to map it to a database table
+@Entity
+@Table(name = "stores")
+public class Store {
+
+    // Primary key attribute that is mapped to the id column of the stores table
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // Auto-generates unique IDs for each store
+    private long id;
+
+
+    // Column annotation to specify the name of the column in the database table
+    @Column(nullable = false, length = 100) // Name of the store cannot be null and has a maximum length of 100 characters
+    private String name;
+
+    // One store can have multiple computers
+    @OneToMany(mappedBy = "tienda", cascade = CascadeType.ALL, orphanRemoval = true) 
+    private List<Computer> computers;
+
+    // Getters and Setters for accessing and modifying the attributes of the Store class
+
+    // Returns the stores's ID
+    public long getId() {
+        return id;
+    }
+
+    // Returns the store's name
+    public String getNombre() {
+        return name;
+    }
+
+    // Returns the list of computers in the store
+    public List<Computer> getComputadoras() {
+        return computers;
+    }
+
+    // Sets the store's ID
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    // Sets the store's name
+    public void setNombre(String name) {
+        this.name = name;
+    }
+
+    // Sets the list of computers in the store
+    public void setComputadoras(List<Computer> computers) {
+        this.computers = computers;
+    }
+
+}
